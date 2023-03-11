633642b85b0bf30b0f99e576f759dfd4bc73dc24
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2023-03-11 09:43:53 +0000

    converted to PyTorch Lightning

--- Code/model.py
+++ Code/model.py
@@ -5,50 +5,52 @@ from torch import optim
 import torch
 import time
 import torch.nn.functional as F
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+import pytorch_lightning as pl
 
 
-class FGSBIR_Model(nn.Module):
+class FGSBIR_Model(pl.LightningModule):
     def __init__(self, hp):
         super(FGSBIR_Model, self).__init__()
         self.sample_embedding_network = eval(hp.backbone_name + '_Network(hp)')
         self.loss = nn.TripletMarginLoss(margin=0.2)
         self.sample_train_params = self.sample_embedding_network.parameters()
-        self.optimizer = optim.Adam(self.sample_train_params, hp.learning_rate)
         self.hp = hp
 
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hp.learning_rate)
+        return optimizer
 
-    def train_model(self, batch):
-        self.train()
-        self.optimizer.zero_grad()
+    def training_step(self, batch, batch_idx):
 
-        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
-        negative_feature = self.sample_embedding_network(batch['negative_img'].to(device))
-        sample_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
+        positive_feature = self.sample_embedding_network(batch['positive_img'])
+        negative_feature = self.sample_embedding_network(batch['negative_img'])
+        sample_feature = self.sample_embedding_network(batch['sketch_img'])
 
         loss = self.loss(sample_feature, positive_feature, negative_feature)
-        loss.backward()
-        self.optimizer.step()
-
-        return loss.item() 
-
-    def evaluate(self, datloader_Test):
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        sketch_feat, positive_feat = self.test_forward(batch)
+        return sketch_feat, positive_feat, batch['sketch_path'], batch['positive_path']
+    
+    def validation_epoch_end(self, validation_step_outputs):
         Image_Feature_ALL = []
         Image_Name = []
         Sketch_Feature_ALL = []
         Sketch_Name = []
-        start_time = time.time()
-        self.eval()
-        for i_batch, sanpled_batch in enumerate(datloader_Test):
-            sketch_feature, positive_feature= self.test_forward(sanpled_batch)
-            Sketch_Feature_ALL.extend(sketch_feature)
-            Sketch_Name.extend(sanpled_batch['sketch_path'])
+        
+        for sketch_feat, positive_feat, sketch_path, positive_path in validation_step_outputs:
+            Sketch_Feature_ALL.extend(sketch_feat)
+            Sketch_Name.extend(sketch_path)
 
-            for i_num, positive_name in enumerate(sanpled_batch['positive_path']):
+            for i_num, positive_name in enumerate(positive_path):
                 if positive_name not in Image_Name:
-                    Image_Name.append(sanpled_batch['positive_path'][i_num])
-                    Image_Feature_ALL.append(positive_feature[i_num])
-
+                    Image_Name.append(positive_path[i_num])
+                    Image_Feature_ALL.append(positive_feat[i_num])
+        
         rank = torch.zeros(len(Sketch_Name))
         Image_Feature_ALL = torch.stack(Image_Feature_ALL)
 
@@ -66,13 +68,14 @@ class FGSBIR_Model(nn.Module):
         top1 = rank.le(1).sum().numpy() / rank.shape[0]
         top10 = rank.le(10).sum().numpy() / rank.shape[0]
 
-        print('Time to EValuate:{}'.format(time.time() - start_time))
-        return top1, top10
+        self.log('top1', top1)
+        self.log('top10', top10)
+        print ('Evaluation metrics: Top1 %.4f Top10 %.4f'%(top1, top10))
 
     def test_forward(self, batch):            #  this is being called only during evaluation
-        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
-        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
-        return sketch_feature.cpu(), positive_feature.cpu()
+        sketch_feature = self.sample_embedding_network(batch['sketch_img'])
+        positive_feature = self.sample_embedding_network(batch['positive_img'])
+        return sketch_feature, positive_feature
 
 
 
